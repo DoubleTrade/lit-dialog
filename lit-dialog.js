9215058c2e68708196f46787286d797442962695
Laurent ZVARA <lzvara@doubletrade.com>
2019-10-07 17:38:55 +0200

    add more properties (closeOnClickOutside, closeIcon, primaryAction, primaryActionLabel, secondaryAction, secondaryActionLabel)

--- lit-dialog.js
+++ lit-dialog.js
@@ -1,12 +1,36 @@
 import { LitElement, css } from 'lit-element';
 import { html, render } from 'lit-html';
 
+import '@polymer/paper-button/paper-button';
+import '@polymer/iron-icons/iron-icons';
+import '@polymer/paper-icon-button/paper-icon-button';
+
 class LitDialog extends LitElement {
   static get properties() {
     return {
+      /**
+       * opened (Boolean) [false] - Open/close the dialog depending on value (default to false)
+       * */
       opened: { type: Boolean },
+      /**
+       * title (String) - Title of the dialog
+       * */
       title: { type: String },
+      /**
+       * closeOnEsc (Boolean) - Close on Pressing ESCAPE key  (default to false)
+       * */
       closeOnEsc: { type: Boolean },
+      /**
+       * closeOnClickOutside (Boolean) - Close on Clicking outside  (default to false)
+       * */
+      closeOnClickOutside: { type: Boolean },
+      /**
+       * closeIcon (Boolean) - Display a close icon (default to false)
+       * */
+      closeIcon: { type: Boolean },
+      /**
+       * html (0bject) - Content to display, you can put a String, or a TemplateResult, if a string is supplied, returned
+       * */
       html: {
         type: Object,
         converter: (value) => {
@@ -14,17 +38,29 @@ class LitDialog extends LitElement {
             return html`${value}`
           }
         },
-      }
+      },
+      primaryAction: { type: Boolean },
+      primaryActionLabel: { type: String },
+      secondaryAction: { type: Boolean },
+      secondaryActionLabel: { type: String },
     };
   }
 
   constructor() {
     super();
     this.opened = false;
-    this.closeOnEsc = true;
+    this.closeOnEsc = false;
+    this.closeIcon = false;
+    this.closeOnClickOutside = false;
     this.title = '';
     this.watchEscapeKey = this.watchEscapeKey.bind(this);
-    this.html = '';
+    this.html = null;
+
+    this.primaryAction = false;
+    this.primaryActionLabel = 'Ok';
+
+    this.secondaryAction = false;
+    this.secondaryActionLabel = 'Cancel';
   }
 
   updated(properties) {
@@ -51,40 +87,105 @@ class LitDialog extends LitElement {
   }
 
   dialogTemplate() {
+    const closeIcon = (this.closeIcon) ? html`
+      <style>
+        paper-icon-button {
+          float: right;
+        }
+      </style>
+      <paper-icon-button icon="close" @tap=${this.close.bind(this)}=></paper-icon-button>
+    ` : null;
+
     const title = (this.title) ? html`
       <style>
-        .lit-dialog_header {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
+        .lit-dialog_title {
+          float: left;
+          font-size: 17px;
+          color: black;
+        }
+      </style>
+      <h2 class="lit-dialog_title">${this.title}</h2>
+    ` : null;
+
+    const header = html`
+      <style>
+          .lit-dialog_header {
+          display: block;
+          overflow: hidden;
           padding: 15px;
           margin-bottom: 20px;
           border-bottom: 1px solid #e0e0e0;
         }
-
-        .lit-dialog_title {
-          font-size: 17px;
-          margin: 0;
-          color: black;
+        .lit-dialog_header:empty {
+          display none;
         }
       </style>
       <div class="lit-dialog_header">
-        <h2 class="lit-dialog_title">${this.title}</h2>
+        ${title}
+        ${closeIcon}
+      <div>
+    `;
+
+    const actionButtons = (label, clickHandler) => html`
+      <style>
+        .lit-dialog_buttons {
+          float: right;
+        }
+      </style>
+      <paper-button
+        class="lit-dialog_buttons"
+        ?raised="${true}"
+        @click="${(e) => clickHandler(e)}"
+      >
+        ${label}
+      </paper-button>
+    `;
+
+    const footer = html`
+      <style>
+        .lit-dialog_footer {
+          position: absolute;
+          bottom: 0px;
+          right: 0px;
+          margin-bottom: 10px;
+          margin-right: 10px;
+          display: block
+          overflow: hidden;
+        }
+        .lit-dialog_footer:empty {
+          display none;
+        }
+      </style>
+      <div class="lit-dialog_footer">
+          ${(this.primaryAction) ? actionButtons(this.primaryActionLabel, () => this.handlePrimaryAction()) : null}
+          ${(this.secondaryAction) ? actionButtons(this.secondaryActionLabel, () => this.handleSecondaryAction()) : null}
       </div>
-    ` : html``;
-    console.warn('render: ', this.html);
-    const htmlTemplate = (this.html) ? this.html : html``;
+    `;
+
+    const htmlTemplate = (this.html) ? html`
+      <style>
+        .lit-dialog_content {
+          display: block;
+          overflow: hidden;
+        }
+      </style>
+      <div class="lit-dialog_content">
+        ${this.html}
+      </div>` : null;
+
     return html`
     <style>
       :host {
         display: block;
       }
 
-      .lit-dialog_wrapper {
+      .lit-dialog {
         align-items: center;
         display: flex;
+        flex-direction: column;
+        margin: auto;
         justify-content: center;
-        position: fixed;
+        position: absolute;
         top: 0;
         left: 0;
         right: 0;
@@ -93,33 +194,45 @@ class LitDialog extends LitElement {
 
       .lit-dialog_overlay {
         background: rgba(0, 0, 0, 0.5);
-        height: 100%;
-        position: fixed;
+        position: absolute;
         top: 0;
         right: 0;
         bottom: 0;
         left: 0;
+        height: 100%;
         width: 100%;
-        margin: 0;
       }
 
-      .lit-dialog_content {
-        min-width: 300px;
-        min-height: 150px;
+      .lit-dialog_wrapper {
+        border-radius: 4px;
+        min-width: 33%;
+        min-height: 25%;
+        max-width: 90%;
+        max-height: 90%;
         background: rgb(255, 255, 255);
         position: fixed;
         box-shadow: 0px 0px 32px 0px rgba(0, 0, 0, 0.5);
       }
     </style>
-    <div class="lit-dialog_wrapper">
-      <div class="lit-dialog_overlay" @click="${this.close}"></div>
-      <div class="lit-dialog_content">
-        ${title}
+    <div class="lit-dialog">
+      <div class="lit-dialog_overlay" @click="${() => this.watchClickOutside()}"></div>
+      <div class="lit-dialog_wrapper">
+        ${(this.title || this.closeIcon) ? header : null}
         ${htmlTemplate}
+        ${footer}
       </div>
     </div>
     `;
   }
+
+  handlePrimaryAction(e) {
+    this.dispatchEvent(new CustomEvent('primary-action-clicked'));
+  }
+
+  handleSecondaryAction(e) {
+    this.dispatchEvent(new CustomEvent('secondary-action-clicked'));
+  }
+
 
   createDialogOverlayDiv() {
     const overlay = document.createElement('div');
@@ -135,14 +248,14 @@ class LitDialog extends LitElement {
 
   removeDialog() {
     this.removeEventListeners();
-    if (!document.querySelector('#lit-dialog-overlay')) {
-      return
+    if (document.querySelector('#lit-dialog-overlay')) {
+      document.body.removeChild(document.querySelector('#lit-dialog-overlay'));
     }
-    document.body.removeChild(document.querySelector('#lit-dialog-overlay'))
   }
 
   setEventListeners() {
     document.addEventListener('keydown', this.watchEscapeKey);
+
   }
 
   removeEventListeners() {
@@ -177,6 +290,12 @@ class LitDialog extends LitElement {
    */
   watchEscapeKey(event) {
     if (event.key === 'Escape' && this.closeOnEsc) {
+      this.close();
+    }
+  }
+
+  watchClickOutside() {
+    if (this.closeOnClickOutside) {
       this.close();
     }
   }
