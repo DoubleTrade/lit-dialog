8679b7f9bec1759a4595285b6fe60a16c82fb4c1
David Rousseau <drousseau@doubletrade.com>
2021-06-29 16:44:07 +0200

    feat: added border-radius mixin on the wrapper

--- lit-dialog.ts
+++ lit-dialog.ts
@@ -7,7 +7,6 @@ import {
 import { html, render } from 'lit-html';
 
 import { TemplateResult } from 'lit-html/lib/template-result';
-
 import './lit-dialog-title';
 import './lit-dialog-close-icon';
 import './lit-dialog-button';
@@ -189,7 +188,7 @@ export default class LitDialog extends LitElement {
         .lit-dialog_wrapper {
           background-color: var(--lit-dialog-background-color);
           overflow: auto;
-          border-radius: 4px;
+          border-radius: var(--lit-dialog-border-radius, 4px);
           max-width: 90%;
           max-height: 90%;
           position: fixed;
